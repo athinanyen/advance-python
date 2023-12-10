2ea4b0e62ac457749f9abd5ea031712156097943
athinanyen <anguyen2478@gmail.com>
2023-12-10 10:47:01 -0600

    code

---
+++ turtle_.py
@@ -0,0 +1,8 @@
+import turtle
+my_turtle=turtle.Turtle()
+for i in range(2):
+    my_turtle.forward(100)
+    my_turtle.right(90)
+    my_turtle.forward(50)
+    my_turtle.right(90)
+turtle.done()                
