d8e3c38cb6e943cc1532d629a9cc32e4187a1c76
athinanyen <anguyen2478@gmail.com>
2023-12-17 10:55:26 -0600

    code

--- turtle_.py
+++ turtle_.py
@@ -1,8 +1,17 @@
-import turtle
+# import turtle
+# my_turtle=turtle.Turtle()
+# for i in range(2):
+#     my_turtle.forward(100)
+#     my_turtle.right(90)
+#     my_turtle.forward(50)
+#     my_turtle.right(90)
+# turtle.done()                
+
 my_turtle=turtle.Turtle()
-for i in range(2):
+for i in range(8):
     my_turtle.forward(100)
-    my_turtle.right(90)
-    my_turtle.forward(50)
-    my_turtle.right(90)
-turtle.done()                
+    my_turtle.right(120)
+    my_turtle.forward(100)
+    my_turtle.right(60)
+    my_turtle.forward(200)
+turtle.done()
